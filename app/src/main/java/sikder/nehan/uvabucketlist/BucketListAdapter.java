9137d095cb151a9fe36c161dd79d3e4e2a9108e0
NehanSikder <ans2ur@virginia.edu>
2017-01-29 20:29:03 -0500

    Finished coding the adapter. Havent tested the code yet

--- app/src/main/java/sikder/nehan/uvabucketlist/BucketListAdapter.java
+++ app/src/main/java/sikder/nehan/uvabucketlist/BucketListAdapter.java
@@ -1,8 +1,65 @@
 package sikder.nehan.uvabucketlist;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+
+import java.util.List;
+
 /**
  * Created by nehan on 1/29/17.
  */
 
-public class BucketListAdapter {
+public class BucketListAdapter extends
+        RecyclerView.Adapter<BucketListAdapter.ViewHolder>  {
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        private CheckBox mCheckBox;
+        public ViewHolder(View itemView) {
+            super(itemView);
+            mCheckBox = (CheckBox) itemView.findViewById(R.id.check);
+        }
+
+    }
+
+    private List<BucketItem> mItems;
+    private Context mContext;
+
+    public BucketListAdapter(Context context, List<BucketItem> items){
+        mItems = items;
+        mContext = context;
+    }
+
+    private Context getContext() {
+        return mContext;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        View itemtView = inflater.inflate(R.layout.item, parent, false);
+
+        // Return a new holder instance
+        ViewHolder viewHolder = new ViewHolder(itemtView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(BucketListAdapter.ViewHolder holder, int position) {
+        BucketItem item = mItems.get(position);
+        // Set item views based on your views and data model
+        CheckBox check = holder.mCheckBox;
+        check.setText(item.getName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return mItems.size();
+    }
 }
--- app/src/main/java/sikder/nehan/uvabucketlist/list.java
+++ app/src/main/java/sikder/nehan/uvabucketlist/list.java
@@ -4,12 +4,19 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+
 public class list extends AppCompatActivity {
+
+    ArrayList<BucketItem> item;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,10 +30,15 @@ public class list extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                //Start new activity
             }
         });
+
+        RecyclerView rvItem = (RecyclerView) findViewById(R.id.items);
+        item = BucketItem.createInitialBucketList();
+        BucketListAdapter adapter = new BucketListAdapter(this, item);
+        rvItem.setAdapter(adapter);
+        rvItem.setLayoutManager(new LinearLayoutManager(this));
     }
 
     @Override
